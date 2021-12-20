f7c92d328be7bf5599d3c65cfc402a3c31a9f921
翩若惊鸿影 <935668768@qq.com>
2021-12-21 00:10:08 +0800

    add memory optimization

--- model.py
+++ model.py
@@ -44,6 +44,8 @@ def load_dataset(DATA_PATH):
     feats = [f for f in train.columns if f not in ['n_2.1', 'n2.2', 'n2.3', 'isDefault']]
     # train = train[feats]
     test = test[feats]
+    reduce_mem_usage(train)
+    reduce_mem_usage(test)
     print('train.shape', train.shape)
     print('test.shape', test.shape)
 
@@ -260,6 +262,41 @@ class MeanEncoder:
 # ==========
 # Fzq's part
 # ==========
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum()
+    print('内存占用{:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum()
+    print('优化后内存为: {:.2f} MB'.format(end_mem))
+    print('内存使用减少 {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    return df
+
 def employmentLength_trans(x):
     if x == r'\N' or x == -999 or x == '-999':
         return -999
